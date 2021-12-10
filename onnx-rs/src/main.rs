c1e5b38e2406f7c1abe41a749869850da9c6f5de
philschmid <schmidphilipp1995@gmail.com>
2021-12-10 13:26:26 +0100

    wip onnx-rs

---
+++ onnx-rs/src/main.rs
@@ -0,0 +1,8 @@
+use onnxruntime::environment::Environment;
+use onnxruntime::ndarray::prelude::*;
+use onnxruntime::ndarray::{IxDyn, OwnedRepr};
+use onnxruntime::{session::Session, GraphOptimizationLevel, LoggingLevel};
+
+fn main() {
+    println!("Hello, world!");
+}
