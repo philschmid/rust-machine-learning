dc354a2d5924d0816bb63787bcc51d97d0117a27
philschmid <schmidphilipp1995@gmail.com>
2022-01-17 21:08:03 +0000

    working example with external handler

--- axum-inference-rs/python/handler.py
+++ axum-inference-rs/python/handler.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 
 class Handler(object):
-    def __init__(self, task):
+    def __init__(self, task, **kwargs):
         self.pipeline = pipeline(task)
 
     def __call__(self, inputs):
--- axum-inference-rs/src/main.rs
+++ axum-inference-rs/src/main.rs
@@ -1,4 +1,7 @@
-use axum::{http::Method, response::IntoResponse, routing::post, AddExtensionLayer, Json, Router};
+use axum::{
+    extract::Extension, http::Method, response::IntoResponse, routing::post, AddExtensionLayer,
+    Json, Router,
+};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 
@@ -13,37 +16,57 @@ pub struct PredictRequest {
     inputs: String,
 }
 
-#[derive(Serialize, Debug)]
-struct PredictResponse {
-    value: f32,
+#[derive(FromPyObject, Debug, Serialize)]
+struct TextClassificationResponse {
+    #[pyo3(item)]
+    label: String,
+    #[pyo3(item)]
+    score: f32,
 }
 
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::IntoPyDict};
 use std::time::{Duration, Instant};
 
-fn load_python_module_from_file() {
-    let now = Instant::now();
-    const PYTHON_MODULE: &str = include_str!("../python/handler.py");
-    println!("Loading python as string {}", now.elapsed().as_millis());
+// fn load_python_module_from_file() {
+//     let now = Instant::now();
+//     println!("Loading python as string {}", now.elapsed().as_millis());
 
-    Python::with_gil(|py| {
-        println!("Loading python with gil {}", now.elapsed().as_millis());
-        let pipeline_file =
-            PyModule::from_code(py, PYTHON_MODULE, "handler.py", "pipeline").unwrap();
-        println!("Loading python module {}", now.elapsed().as_millis());
-        let handler = pipeline_file.getattr("Handler").unwrap();
-        println!("Loading handler module {}", now.elapsed().as_millis());
-        let handler = handler.call1(("text-classification",)).unwrap();
-        println!("Loading init handler {}", now.elapsed().as_millis());
+//     let handler = Python::with_gil(|py| {
+//         println!("Loading python with gil {}", now.elapsed().as_millis());
+//         let pipeline_file =
+//             PyModule::from_code(py, PYTHON_MODULE, "handler.py", "Handler").unwrap();
+//         println!("Loading python module {}", now.elapsed().as_millis());
+//         let handler = pipeline_file
+//             .getattr("Handler")
+//             .unwrap()
+//             .call1(("text-classification",))
+//             .unwrap();
 
-        let pred = handler.call_method1("__call__", ("i love you.",)).unwrap();
-        println!("predict {}", now.elapsed().as_millis());
-        println!("{}", pred)
-    })
-}
+//         let pred = handler.call_method1("__call__", ("i love you.",)).unwrap();
+//         println!("predict {}", now.elapsed().as_millis());
+//         println!("{}", pred)
+//     });
+// }
 
-async fn predict_route() -> impl IntoResponse {
-    Json(PredictResponse { value: 2.0 })
+async fn predict_route(
+    Json(payload): Json<PredictRequest>,
+    Extension(handler): Extension<Py<PyAny>>,
+) -> impl IntoResponse {
+    // println!("{:?}", payload);
+    // let now = std::time::Instant::now();
+    let pred = Python::with_gil(|py| {
+        // println!("loading python {}", now.elapsed().as_millis());
+        // let now = std::time::Instant::now();
+        let res = handler
+            .call1(py, (payload.inputs.as_str(),))
+            .unwrap()
+            .extract::<Vec<TextClassificationResponse>>(py)
+            .unwrap();
+        // println!("prediction took: {}ms", now.elapsed().as_millis());
+        res
+    });
+    // println!("{:?}", pred);
+    Json(pred)
 }
 
 #[tokio::main]
@@ -55,34 +78,47 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // init request tracing for `TraceLayer`
     tracing_subscriber::fmt::init();
 
-    load_python_module_from_file();
+    let text_classification: Py<PyAny> = Python::with_gil(|py| {
+        let kwargs = [("device", 0)].into_py_dict(py);
+        const PYTHON_MODULE: &str = include_str!("../python/handler.py");
 
-    // // middleware stack
-    // let middleware_stack = ServiceBuilder::new()
-    //     .layer(TraceLayer::new_for_http())
-    //     .layer(
-    //         // see https://docs.rs/tower-http/latest/tower_http/cors/index.html
-    //         // for more details
-    //         CorsLayer::new()
-    //             .allow_origin(any())
-    //             .allow_methods(vec![Method::POST, Method::OPTIONS])
-    //             .allow_headers(any()),
-    //     )
-    //     .layer(AddExtensionLayer::new(tx.clone()));
+        let pipeline_file = PyModule::from_code(py, PYTHON_MODULE, "handler.py", "Handler")
+            .expect("Handler should be loaded");
+        pipeline_file
+            .getattr("Handler")
+            .unwrap()
+            .call(("text-classification",), Some(kwargs))
+            .unwrap()
+            .extract()
+            .unwrap()
+    });
 
-    // // build our application with a route
-    // let app = Router::new()
-    //     .route("/predict", post(predict_route))
-    //     .layer(middleware_stack);
+    // middleware stack
+    let middleware_stack = ServiceBuilder::new()
+        .layer(TraceLayer::new_for_http())
+        .layer(
+            // see https://docs.rs/tower-http/latest/tower_http/cors/index.html
+            // for more details
+            CorsLayer::new()
+                .allow_origin(any())
+                .allow_methods(vec![Method::POST, Method::OPTIONS])
+                .allow_headers(any()),
+        )
+        .layer(AddExtensionLayer::new(text_classification.clone()));
 
-    // // run it with hyper on localhost:3000
-    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    // tracing::debug!("listening on {}", addr);
-    // axum::Server::bind(&addr)
-    //     .http1_preserve_header_case(true)
-    //     .http1_title_case_headers(true)
-    //     .serve(app.into_make_service())
-    //     .await
-    //     .unwrap();
+    // build our application with a route
+    let app = Router::new()
+        .route("/predict", post(predict_route))
+        .layer(middleware_stack);
+
+    // run it with hyper on localhost:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .http1_preserve_header_case(true)
+        .http1_title_case_headers(true)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
     Ok(())
 }
